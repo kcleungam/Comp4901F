5c47901baa58e9204639006f4af466add23c879b
kcleungam <kcleungam@ust.hk>
2015-11-21 21:39:30 +0800

    Ready to code

--- Flitter.java
+++ Flitter.java
@@ -5,4 +5,23 @@ public class Flitter {
     public static void main(String args[]) {
 
     }
+
+    public AccountList readName(String fileName, AccountList accountList){
+
+        //TODO Use loop and filestream to add data to each account and then put it in account list
+        Account account = new Account();
+        /// loop end here
+
+        accountList.addAccount(account);
+        return accountList;
+    }
+
+    public AccountList readLivingPlace(String fileName, AccountList accountList){
+
+        //TODO Use loop and filestream to add data to each account and then put it in account list
+        Account account = new Account();
+        /// loop end here
+        return accountList;
+    }
+
 }
